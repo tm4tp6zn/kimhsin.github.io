f871408738674401e2ff233a3c6935f8f5aaf5f8
tm4tp6zn <154357879+tm4tp6zn@users.noreply.github.com>
2024-07-14 23:46:42 +0800

    JS V2
    Signed-off-by: tm4tp6zn <154357879+tm4tp6zn@users.noreply.github.com>

--- js/js.js
+++ js/js.js
@@ -1,1 +1,11 @@
-xzxz
+const dataToggle = document.querySelectorAll("[data-toggle]");
+const menuToggle = document.getElementById("navbarSupportedContent");
+const bsCollapse = new bootstrap.Collapse(menuToggle, {
+  toggle: false,
+});
+
+dataToggle.forEach((item) => {
+  item.addEventListener("click", () => {
+    bsCollapse.toggle();
+  });
+});
